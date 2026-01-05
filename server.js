8735f322b9cee02a400bc61c8fbf6f5859619e93
HuynhQuangNam2280601992 <huynhquangnam3105@gmail.com>
2026-01-05 21:02:09 +0700

    started project

---
+++ server.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+require('module-alias/register');
+
+const express = require("express");
+const cors = require("cors");
+// const appRoute = require("@/routes");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+console.log(process.env);
+
+
+const origins = ["http://localhost:5173", "https://boybuonme.github.io"];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, false);
+    if (origins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Fail"), false);
+    }
+  },
+};
+
+app.options("/api/", cors(corsOptions));
+
+// app.get("/test-success", (req, res) => {
+//   res.success(
+//     {
+//       message: "Hello World",
+//     },
+//     200
+//   );
+// });
+
+// app.get("/test-error", (req, res) => {
+//   throw new Error("Fail");
+//   res.error(400, "Task not found", null);
+// });
+
+// app.use("/api", appRoute);
+
+
+app.listen(port, () => {
+  console.log("Running on 127.0.0.1:" + port);
+});
